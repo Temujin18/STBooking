fda61deb35fb3b2ae9252e7be76eeffd8c44426c
John Exim <jonh.exim.ece@gmail.com>
2020-07-28 14:50:03 +0800

    Initial commit for making forms; WIP

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 load_dotenv()
 
 DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_NAME = os.environ['DB_NAME']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}'
+app.config['SECRET_KEY'] = SECRET_KEY
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
     return f'<h1>Hello! {DB_USER}</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
