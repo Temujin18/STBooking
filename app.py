d3d6aab97a933eee87d683c442bb1f302dfab056
John Exim <jonh.exim.ece@gmail.com>
2020-07-29 16:04:01 +0800

    Implemented register and login forms for learning

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
@@ -11,6 +11,10 @@ DB_PASSWORD = os.environ['DB_PASSWORD']
 DB_NAME = os.environ['DB_NAME']
 SECRET_KEY = os.environ['SECRET_KEY']
 
+# Room table in postgres has room_type enum with values single, double, triple, quad, queen, king
+# Room table also has room_status enum with values booked, vacant, out order
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}'
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -21,14 +25,22 @@ db = SQLAlchemy(app)
 def index():
     return render_template('layout.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@stbook.com' and form.password.data == 'stbook':
+            flash('You have been logged in!', 'success')
+        else:
+            flash('Login Failed. Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
