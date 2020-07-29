42151ed20490a7af6bd2e247fe8d59e508df4074
John Exim <jonh.exim.ece@gmail.com>
2020-07-29 20:48:50 +0800

    Created sqlalchemy models; Enum types commented out due to postgres fails

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 import os
+import enum
 
 load_dotenv()
 
@@ -20,6 +21,50 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@l
 app.config['SECRET_KEY'] = SECRET_KEY
 
 db = SQLAlchemy(app)
+
+# class RoomType(enum.Enum):
+#     SINGLE = 'single'
+#     DOUBLE = 'double'
+#     TRIPLE = 'triple'
+#     QUAD = 'quad'
+#     QUEEN = 'queen'
+#     KING = 'king'
+
+# class RoomStatus(enum.Enum):
+#     BOOKED = 'booked'
+#     VACANT = 'vacant'
+#     OUT_OF_ORDER = 'out of order'
+
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    guest_booking = db.relationship('Booking', backref='guest', lazy=False)
+
+    def __repr__(self):
+        return f"Guest({self.id}, {self.first_name}, {self.last_name})"
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_type = db.Column(db.String(20), nullable=False)
+    room_status = db.Column(db.String(20), nullable=False)
+    room_booking = db.relationship('Booking', backref='room', lazy=False)
+
+    def __repr__(self):
+        return f"Room({self.id}, {self.room_type}, {self.room_status})"
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Booking({self.id}, {self.room}, {self.guest})"
+
 
 @app.route("/")
 def index():
