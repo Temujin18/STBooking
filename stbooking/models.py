1008937a85b094d88c4a8424a4448216fee75b0c
John Exim <jonh.exim.ece@gmail.com>
2020-08-08 22:15:27 +0800

    Updated password length from 50 to 60 to cater to hashed passwords

--- stbooking/models.py
+++ stbooking/models.py
@@ -50,7 +50,7 @@ class Booking(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
     guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'), nullable=False)
     
 
