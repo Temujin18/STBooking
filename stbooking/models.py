7deefbca1ecae79572605eae80524d6f6612a3a2
John Exim <jonh.exim.ece@gmail.com>
2020-08-09 17:41:26 +0800

    Add AdminAccount model and migrate changes to db

--- stbooking/models.py
+++ stbooking/models.py
@@ -61,3 +61,12 @@ class UserAccount(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User({self.id}, {self.username}, {self.guest_id})"
+
+
+class AdminAccount(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"Admin({self.id}, {self.username})"
